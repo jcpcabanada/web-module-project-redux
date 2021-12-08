606ef37c2ef0e968cc80fdc38b9d19ace268ea5c
jcpcabanada <jcpcabanada@gmail.com>
2021-12-07 19:19:52 -0800

    add movies

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,4 +1,4 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
+import {ADD_MOVIE, DELETE_MOVIE} from '../actions/movieActions.js';
 import movies from './../data.js';
 
 const initialState = {
@@ -7,11 +7,21 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case DELETE_MOVIE:
             return {
                 ...state,
-                movies: state.movies.filter(item=>(action.payload !== item.id))
+                movies: state.movies.filter(item => (action.payload !== item.id))
+            }
+        case ADD_MOVIE:
+            const newMovie = {
+                ...action.payload,
+                id: Date.now()
+            }
+
+            return {
+                ...state,
+                movies: [...state.movies, newMovie]
             }
         default:
             return state;
